36e1bbfbded6f41ffccd922c9c98870cbf2236dc
André Perez <andre_mperez@hotmail.com>
2015-10-16 21:34:25 +0100

    Update matlabPlot.m

--- Convolution/matlabPlot.m
+++ Convolution/matlabPlot.m
@@ -1,7 +1,7 @@
 %   Program: 		    Convolution - PLOT
 %   Author: 		    André Perez
 %   Contact: 		    andre_mperez@hotmail.com
-%   Last Modified: 	  10 October 2015
+%   Last Modified: 	    10 October 2015
 
 close all;
 clear all;
